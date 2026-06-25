a0f9556b777483aa521343e3fb325dd4a2a6150d
VSOFT <VSOFT@VSOFT_MASTER>
2026-06-25 15:04:25 +0700

    Update Form 9 and 10

---
+++ Form10.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblMethod = new Label();
+            CboMethod = new ComboBox();
+            LblAmount = new Label();
+            TxtValue1 = new TextBox();
+            TxtValue2 = new TextBox();
+            label3 = new Label();
+            TxtValue3 = new TextBox();
+            LblTerm = new Label();
+            BtnCaculate = new Button();
+            listViewReport = new ListView();
+            SuspendLayout();
+            // 
+            // LblMethod
+            // 
+            LblMethod.AutoSize = true;
+            LblMethod.Location = new Point(47, 21);
+            LblMethod.Name = "LblMethod";
+            LblMethod.Size = new Size(60, 15);
+            LblMethod.TabIndex = 0;
+            LblMethod.Text = "METHOD:";
+            // 
+            // CboMethod
+            // 
+            CboMethod.FormattingEnabled = true;
+            CboMethod.Location = new Point(113, 21);
+            CboMethod.Name = "CboMethod";
+            CboMethod.Size = new Size(174, 23);
+            CboMethod.TabIndex = 1;
+            // 
+            // LblAmount
+            // 
+            LblAmount.AutoSize = true;
+            LblAmount.Location = new Point(47, 50);
+            LblAmount.Name = "LblAmount";
+            LblAmount.Size = new Size(54, 15);
+            LblAmount.TabIndex = 2;
+            LblAmount.Text = "VALUE 1:";
+            // 
+            // TxtValue1
+            // 
+            TxtValue1.Location = new Point(113, 50);
+            TxtValue1.Name = "TxtValue1";
+            TxtValue1.Size = new Size(174, 23);
+            TxtValue1.TabIndex = 3;
+            // 
+            // TxtValue2
+            // 
+            TxtValue2.Location = new Point(113, 88);
+            TxtValue2.Name = "TxtValue2";
+            TxtValue2.Size = new Size(174, 23);
+            TxtValue2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 4;
+            label3.Text = "VALUE 2:";
+            // 
+            // TxtValue3
+            // 
+            TxtValue3.Location = new Point(113, 127);
+            TxtValue3.Name = "TxtValue3";
+            TxtValue3.Size = new Size(174, 23);
+            TxtValue3.TabIndex = 7;
+            // 
+            // LblTerm
+            // 
+            LblTerm.AutoSize = true;
+            LblTerm.Location = new Point(47, 127);
+            LblTerm.Name = "LblTerm";
+            LblTerm.Size = new Size(54, 15);
+            LblTerm.TabIndex = 6;
+            LblTerm.Text = "VALUE 3:";
+            // 
+            // BtnCaculate
+            // 
+            BtnCaculate.Location = new Point(113, 156);
+            BtnCaculate.Name = "BtnCaculate";
+            BtnCaculate.Size = new Size(174, 52);
+            BtnCaculate.TabIndex = 8;
+            BtnCaculate.Text = "&Caculate";
+            BtnCaculate.UseVisualStyleBackColor = true;
+            BtnCaculate.Click += BtnCaculate_Click;
+            // 
+            // listViewReport
+            // 
+            listViewReport.Location = new Point(113, 214);
+            listViewReport.Name = "listViewReport";
+            listViewReport.Size = new Size(508, 145);
+            listViewReport.TabIndex = 9;
+            listViewReport.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form10
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(672, 382);
+            Controls.Add(listViewReport);
+            Controls.Add(BtnCaculate);
+            Controls.Add(TxtValue3);
+            Controls.Add(LblTerm);
+            Controls.Add(TxtValue2);
+            Controls.Add(label3);
+            Controls.Add(TxtValue1);
+            Controls.Add(LblAmount);
+            Controls.Add(CboMethod);
+            Controls.Add(LblMethod);
+            Name = "Form10";
+            Text = "Using Public Method and paramet";
+            Load += Form10_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblMethod;
+        private ComboBox CboMethod;
+        private Label LblAmount;
+        private TextBox TxtValue1;
+        private TextBox TxtValue2;
+        private Label label3;
+        private TextBox TxtValue3;
+        private Label LblTerm;
+        private Button BtnCaculate;
+        private ListView listViewReport;
+    }
+}
---
+++ Form10.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form10 : Form
+    {
+        public Form10()
+        {
+            InitializeComponent();
+        }
+
+        private void Form10_Load(object sender, EventArgs e)
+        {
+            // Call Combobox Property
+            My_Combobox_Property();
+
+            // Call TextBox Property
+            My_TextBox_Property();
+
+            //Call Method create listview column
+            My_Listview_Col();
+
+        }
+
+        public void My_Combobox_Property()
+        {
+            CboMethod.Items.Clear();
+            CboMethod.Items.Add("SUM");
+            CboMethod.Items.Add("MUL");
+            CboMethod.Items.Add("SUB");
+            CboMethod.Text = CboMethod.Items[0].ToString();
+        }
+
+        public void My_TextBox_Property()
+        {
+            TxtValue1.Text = "0";
+            TxtValue2.Text = "0";
+            TxtValue3.Text = "0";
+        }
+
+        public void My_Listview_Col()
+        {
+            //Create Property in listview control
+
+            listViewReport.View = View.Details;
+            listViewReport.FullRowSelect = true;
+            listViewReport.GridLines = true;
+
+            //Create Col in listview control
+            listViewReport.Columns.Clear();
+            listViewReport.Columns.Add("NO.",50,HorizontalAlignment.Center);
+            listViewReport.Columns.Add("VALUE1", 100, HorizontalAlignment.Center);
+            listViewReport.Columns.Add("VALUE2", 100, HorizontalAlignment.Center);
+            listViewReport.Columns.Add("VALUE3", 100, HorizontalAlignment.Center);
+            listViewReport.Columns.Add("METHOD", 100, HorizontalAlignment.Center);
+            listViewReport.Columns.Add("AMOUNT", 100, HorizontalAlignment.Center);
+        }
+
+        public double My_Method(string Tmp_Method , double VALUE1, double VALUE2, double VALUE3)
+        {
+            double My_calculate = 0;
+
+            switch (Tmp_Method)
+            {
+                case "SUM":
+                    My_calculate = VALUE1+VALUE2+VALUE3;
+                    break;
+
+                case "SUB":
+                    My_calculate = VALUE1-VALUE2-VALUE3;
+                    break;
+
+                case "MUL":
+                    My_calculate=VALUE1* VALUE2*VALUE3;
+                    break;
+
+                default:
+                    //Console.WriteLine("Number must be between 1 and 7.");
+                    //Display message box
+                    break;
+            }
+
+            return My_calculate;
+        }
+
+        public void My_Caculate()
+        {
+            string Tmp_method = CboMethod.Text;
+            double VALUE1 = double.Parse(TxtValue1.Text.ToString());    //  get value from TxtValue1
+            double VALUE2 = double.Parse(TxtValue2.Text.ToString()); ;    //  get value from TxtValue2
+            double VALUE3 = double.Parse(TxtValue3.Text.ToString()); ;    //  get value from TxtValue3
+
+            double My_AMT = My_Method(CboMethod.Text, VALUE1, VALUE2, VALUE3);
+
+            //listViewReport.Items.Clear();
+
+            int My_Num = listViewReport.Items.Count + 1;
+
+            //Delclare ListviewItems
+            ListViewItem items = new ListViewItem(My_Num.ToString());  //NO.
+            items.SubItems.Add(VALUE1.ToString());
+            items.SubItems.Add(VALUE2.ToString());
+            items.SubItems.Add(VALUE3.ToString());
+            items.SubItems.Add(Tmp_method.ToString());
+            items.SubItems.Add(My_AMT.ToString());
+
+            //Get items to Listview
+            listViewReport.Items.Add(items);
+
+            //listViewReport.Items.Add
+        }
+        private void BtnCaculate_Click(object sender, EventArgs e)
+        {
+            My_Caculate();
+        }
+    }
+}
